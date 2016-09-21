ab46742b659c7398f64e350914bc61c3fb18b1fa
mykolademydyuk <mykolademydyuk@gmail.com>
2016-09-21 21:40:45 +0300

    add game

---
+++ NetGames/Tank/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Tank.Map;
+using Tank.GameObject;
+
+namespace Tank
+{
+    class Game : ISerializable
+    {
+        private IMap _currentMap;
+        public IMap CurrentMap { get { return _currentMap; } }
+
+        public IMap Play()
+        {
+            return CurrentMap;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
